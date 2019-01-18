2533db93c44defbcf622c5cf8e8f5f68c3dd093f
Whitny Edwards <wne4e2@gmail.com>
2019-01-18 01:46:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,8 @@ def create_project_hash
  # This just opens a file and reads it into a variable
 html = File.read('fixtures/kickstarter.html')
 
-binding.pry 
 kickstarter = Nokogiri::HTML(html)
 
 end 
 
+binding.pry 
